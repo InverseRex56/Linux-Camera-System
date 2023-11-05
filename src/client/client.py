b32a646c05cb5319d4788e2b3ea9e6c6123190c3
Anhton <anhton154321@gmail.com>
2023-11-05 17:29:05 -0500

    Renamed server folder to network folder. Added dummy data to psql.

--- src/client/client.py
+++ src/client/client.py
@@ -5,14 +5,14 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # Define the server address and port
-server_url = 'http://server:8080'  # Change to the actual server address if it's on a different machine
+network_url = 'http://network:8080'  # Change to the actual server address if it's on a different machine
 
 @app.route('/update_and_send', methods=['GET'])
 def update_and_send_data():
     try:
         with open("config.json", "r") as config_file:
             data_to_send = json.load(config_file)
-            response = requests.post(server_url, json=data_to_send)
+            response = requests.post(network_url, json=data_to_send)
             return response.text
     except FileNotFoundError:
         return "Config file not found."
--- src/server/db/init_tables.sql
+++ src/server/db/init_tables.sql
@@ -1,6 +1,6 @@
 -- This file initiables all tables and their corresponding schemas for our database backend
 
-CREATE TABLE IF NOT EXISTS status(
+CREATE TABLE IF NOT EXISTS camera_status(
     cam_id                 int,
     cam_status             int,
     most_recent_pic        varchar(80)
@@ -12,3 +12,9 @@ CREATE TABLE IF NOT EXISTS events(
     events                 int,
     datetimes              varchar(80)
 );
+
+INSERT INTO camera_status (cam_id, cam_status, most_recent_pic) VALUES (1, 2, 'GMU');
+INSERT INTO camera_status (cam_id, cam_status, most_recent_pic) VALUES (3, 4, 'JMU');
+
+INSERT INTO events (cam_id, events, datetimes) VALUES (5, 6, 'today');
+INSERT INTO events (cam_id, events, datetimes) VALUES (7, 8, 'yesterday');
